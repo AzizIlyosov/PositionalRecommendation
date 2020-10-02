c5c741c0266a867ee99f0dc18fb46c8aeb01f3bc
Ilyosov Aziz <ilyosovaa94@gmail.com>
2020-10-02 22:32:28 +0900

    Open project

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ class main:
             key show  user id while value (list)  shows item id's
             [
                 {2:[2,3,4]}
-            ] means  user 2  had interaction with item 2,3,4
+            ]
+            means  user 2  had interaction with item 2,3,4
         ----------------------------------------------------------------------------------------------------------------
 
         users: list of the user features
