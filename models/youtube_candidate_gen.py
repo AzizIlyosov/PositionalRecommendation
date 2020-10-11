513b83b86a4c2dff5df1a8e07e53c7c644523208
Ilyosov Aziz <ilyosovaa94@gmail.com>
2020-10-11 22:09:53 +0900

    raw model

--- models/youtube_candidate_gen.py
+++ models/youtube_candidate_gen.py
@@ -20,7 +20,7 @@ class model:
 
 
         user_features=[
-        {'name':'Job",  'length'=3, } ,...  ]  #    this means user may have up to least 3 jobs(in
+        {'name':'Job",  'length'=3, } ,...  ]  #    this means user may have up to 3 jobs(in
                                                #    practice you my use as much as you want)
 
 
@@ -42,11 +42,19 @@ class model:
 
         max_history_length = kwargs['max_history_length']
         self.feature =kwargs['feature']
-        self.user_features = kwargs['user_features']
+        if kwargs.get('user_features'):
+            self.user_features = kwargs['user_features']
+        else:self.user_features={}
+        zero = tf.constant(0, tf.int32)
 
         with  tf.variable_scope('youtube_candidate_gen'):
+            # user  embedding
             self.users_embedding = tf.get_variable( 'user_embedding', [ number_of_users , user_emb_length], dtype=tf.float32)
+
+            #item embedding
             self.item_embedding  = tf.get_variable('item_embedding',  [ number_of_items , item_emb_length], dtype=tf.float32)
+
+            # declaring embedding for all categorical features
             self.feature_net_emb = {}
             for i in self.feature:
                 if self.feature[i]['type'] == 'categorical':
@@ -56,14 +64,21 @@ class model:
             self.user_id = tf.placeholder(dtype=tf.int32, shape=[None, ],    name = 'user_id')
             self.history = tf.placeholder(dtype=tf.int32, shape=[None, max_history_length ], name='item_id')
 
+
             self.user_feature_placeholder={}
-            self.mask_features={}
+            self.user_features_embedding_mask={}
+
+
 
             for i in self.user_features:
                 if self.feature_net_emb.get(i): # this means feature is categorical because there
                                                 # is embedding for this feature
-
                     a = tf.placeholder( dtype=tf.int32,   shape = [None, self.user_features[i]['length']] ,name='feature_id_'+i  )
+                    # print('emb length: ', self.feature[i]['emb_length'])
+                    mask = tf.tile( tf.expand_dims(  tf.cast(tf.not_equal(a,zero), tf.float32) , axis=-1)  ,[1, 1, self.feature[i]['emb_length'] ] , name='feature_emb_mask_matrix_'+i)
+                    # print('this is shape of the mask: ', mask.shape )
+                    self.user_features_embedding_mask.update({i:mask})
+
                 else:
                     a = tf.placeholder( dtype=tf.float32, shape = [None, self.user_features[i]['length']] ,name='feature_id_'+i  )
 
@@ -72,14 +87,18 @@ class model:
             # before getting average of features , 0 embedding should be  removed
             # firstly,  indexes are turned into bool,
             # then they are converted to int again
+            self.user_embedding =  tf.gather(params=self.users_embedding, indices= self.user_id )
+            history_mask = tf.tile( tf.expand_dims( tf.cast( tf.not_equal(self.history, zero) , tf.float32), axis=-1) ,[1,1,item_emb_length], name='history_mask' )
+            self.hist_average  = tf.reduce_mean( tf.multiply( tf.gather(params=self.item_embedding, indices=self.history ) , history_mask  ) , axis=-2, name= 'average_history_emb')
+            print('this  is shape of the histry : ', self.hist_average.shape)
+            print('this  is shape of the user embedding ', self.user_embedding.shape)
 
-
-
-
-
-            user_feature_emb_gather={}
+            self.user_feature_emb_gather={}
             for i in self.feature_net_emb:
-                user_feature_emb_gather.update({i:tf.gather(params=self.feature_net_emb[i], indices=self.user_feature_placeholder[i], name='gather_'+i )})
+                c= tf.gather(params=self.feature_net_emb[i], indices=self.user_feature_placeholder[i], name='embedding_lookup__'+i )
+                #c =c * self.user_features_embedding_mask[i]
+                c = tf.multiply(c, self.user_features_embedding_mask[i], name = 'masking_'+i)
+                self.user_feature_emb_gather.update({i:c})
 
 
 
@@ -88,8 +107,11 @@ class model:
 
 
 
-            for i in user_feature_emb_gather:
-                user_feature_emb_gather[i] = tf.reduce_mean( user_feature_emb_gather[i] , axis=1, name='mean_feature_'+i)
+            for i in self.user_feature_emb_gather:
+                self.user_feature_emb_gather[i] = tf.reduce_mean( self.user_feature_emb_gather[i] , axis=1, name='mean_feature_'+i)
+                print('shape of the feature  embedding '+i+' : ', self.user_feature_emb_gather[i].shape)
+
+
 
 
 
@@ -104,7 +126,7 @@ class model:
         #               feed_dict={
         #
         #               })
-        print(em)
+
         print('model has trained ....')
 
 
